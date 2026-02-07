c5a1e19d8140bf7433ce9bb7ffc19b9b0bdf69d0
Aimee Hanson <aimee.hanson@bristol.ac.uk>
2026-02-07 19:10:27 +0000

    QC fields extraction

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # ukbrap-ieu-gwas
+
+## Running a GWAS with UKB Genotypes
+
+### 1. Cohort selection and sample QC
+**QC fields:**\
+p22001 - inferred gender\
+p31 - reported gender\
+p22019 - sex chromosome aneuploidy\
+p22027 - heterozygosity and missingness rate outliers
+
+**Ancestry restrictions:**\
+p21000 - self reported ancestry
+
+**Relatedness:**
+`data.minimal_relateds.plink.txt` - maximum unrelated set of individuals
+`data.highly_relateds.[software].txt` - highly related samples to exclude
+
+
+
+
+
+
+### 2. Extracting phenotypes/covariates
+
+### 3. Merging genotype data
+
+### 4. Running model
+
+
---
+++ scripts/01_pullQCfields.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Purpose: Extract fields for UKB participants based on list of field names
+
+# This script interacts with the UK Biobank Research Analysis Platform (RAP) via the command line
+# using dx-toolkit. To access UKB RAP data and software you must first be logged into the platform
+# with <dx-login> and have selected the appropriate project with <dx-select>
+
+
+
+# Run:
+# run using: ./01_pullQCfields.sh
+
+# Inputs:
+# QCfields.txt (List of field IDs)
+
+# Output:
+# ukb_QCfields.tsv
+
+# Ensure required input fields are uploaded to your files_dir in the RAP space prior to running using
+# dx upload ukb_QCfields.tsv --path=${files_dir}
+
+# ======================================================== #
+
+source ../.env
+
+dx run table-exporter \
+	-idataset_or_cohort_or_dashboard="${dataset}" \
+	-ioutput="ukb_QCfields" \
+	-ioutput_format="TSV" \
+	-icoding_option="REPLACE" \
+	-iheader_style="FIELD-NAME" \
+	-ientity="participant" \
+	-ifield_names_file_txt="${files_dir}/QCfields.txt" \
+	--instance-type "mem1_ssd1_v2_x4" \
+	--destination="${project}:${data_dir}" \
+	--priority="high" \
+	--brief --yes
